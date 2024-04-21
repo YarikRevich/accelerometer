e64989a91290bb53df5d8ff726b016627880fe7c
Yaroslav Svitlytskyi <yariksvitlitskiy81@gmail.com>
2024-04-21 17:46:43 +0200

    feature: initial commit

---
+++ cmake-build-debug/CMakeFiles/accelerometer.elf.dir/DependInfo.cmake
@@ -0,0 +1,63 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "ASM"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_ASM
+  "/Users/yaroslavsvitlytskyi/STM32CubeIDE/workspace_1.14.0/accelerometer/Core/Startup/startup_stm32l476rgtx.s" "/Users/yaroslavsvitlytskyi/STM32CubeIDE/workspace_1.14.0/accelerometer/cmake-build-debug/CMakeFiles/accelerometer.elf.dir/Core/Startup/startup_stm32l476rgtx.s.obj"
+  )
+set(CMAKE_ASM_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_ASM
+  "DEBUG"
+  "STM32L476xx"
+  "USE_HAL_DRIVER"
+  )
+
+# The include file search paths:
+set(CMAKE_ASM_TARGET_INCLUDE_PATH
+  "/Users/yaroslavsvitlytskyi/STM32CubeIDE/workspace_1.14.0/accelerometer/Core/Inc"
+  "/Users/yaroslavsvitlytskyi/STM32CubeIDE/workspace_1.14.0/accelerometer/Drivers/STM32L4xx_HAL_Driver/Inc"
+  "/Users/yaroslavsvitlytskyi/STM32CubeIDE/workspace_1.14.0/accelerometer/Drivers/STM32L4xx_HAL_Driver/Inc/Legacy"
+  "/Users/yaroslavsvitlytskyi/STM32CubeIDE/workspace_1.14.0/accelerometer/Drivers/CMSIS/Device/ST/STM32L4xx/Include"
+  "/Users/yaroslavsvitlytskyi/STM32CubeIDE/workspace_1.14.0/accelerometer/Drivers/CMSIS/Include"
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/yaroslavsvitlytskyi/STM32CubeIDE/workspace_1.14.0/accelerometer/Core/Src/main.c" "CMakeFiles/accelerometer.elf.dir/Core/Src/main.c.obj" "gcc" "CMakeFiles/accelerometer.elf.dir/Core/Src/main.c.obj.d"
+  "/Users/yaroslavsvitlytskyi/STM32CubeIDE/workspace_1.14.0/accelerometer/Core/Src/stm32l4xx_hal_msp.c" "CMakeFiles/accelerometer.elf.dir/Core/Src/stm32l4xx_hal_msp.c.obj" "gcc" "CMakeFiles/accelerometer.elf.dir/Core/Src/stm32l4xx_hal_msp.c.obj.d"
+  "/Users/yaroslavsvitlytskyi/STM32CubeIDE/workspace_1.14.0/accelerometer/Core/Src/stm32l4xx_it.c" "CMakeFiles/accelerometer.elf.dir/Core/Src/stm32l4xx_it.c.obj" "gcc" "CMakeFiles/accelerometer.elf.dir/Core/Src/stm32l4xx_it.c.obj.d"
+  "/Users/yaroslavsvitlytskyi/STM32CubeIDE/workspace_1.14.0/accelerometer/Core/Src/syscalls.c" "CMakeFiles/accelerometer.elf.dir/Core/Src/syscalls.c.obj" "gcc" "CMakeFiles/accelerometer.elf.dir/Core/Src/syscalls.c.obj.d"
+  "/Users/yaroslavsvitlytskyi/STM32CubeIDE/workspace_1.14.0/accelerometer/Core/Src/sysmem.c" "CMakeFiles/accelerometer.elf.dir/Core/Src/sysmem.c.obj" "gcc" "CMakeFiles/accelerometer.elf.dir/Core/Src/sysmem.c.obj.d"
+  "/Users/yaroslavsvitlytskyi/STM32CubeIDE/workspace_1.14.0/accelerometer/Core/Src/system_stm32l4xx.c" "CMakeFiles/accelerometer.elf.dir/Core/Src/system_stm32l4xx.c.obj" "gcc" "CMakeFiles/accelerometer.elf.dir/Core/Src/system_stm32l4xx.c.obj.d"
+  "/Users/yaroslavsvitlytskyi/STM32CubeIDE/workspace_1.14.0/accelerometer/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal.c" "CMakeFiles/accelerometer.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal.c.obj" "gcc" "CMakeFiles/accelerometer.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal.c.obj.d"
+  "/Users/yaroslavsvitlytskyi/STM32CubeIDE/workspace_1.14.0/accelerometer/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_cortex.c" "CMakeFiles/accelerometer.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_cortex.c.obj" "gcc" "CMakeFiles/accelerometer.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_cortex.c.obj.d"
+  "/Users/yaroslavsvitlytskyi/STM32CubeIDE/workspace_1.14.0/accelerometer/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_dma.c" "CMakeFiles/accelerometer.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_dma.c.obj" "gcc" "CMakeFiles/accelerometer.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_dma.c.obj.d"
+  "/Users/yaroslavsvitlytskyi/STM32CubeIDE/workspace_1.14.0/accelerometer/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_dma_ex.c" "CMakeFiles/accelerometer.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_dma_ex.c.obj" "gcc" "CMakeFiles/accelerometer.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_dma_ex.c.obj.d"
+  "/Users/yaroslavsvitlytskyi/STM32CubeIDE/workspace_1.14.0/accelerometer/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_exti.c" "CMakeFiles/accelerometer.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_exti.c.obj" "gcc" "CMakeFiles/accelerometer.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_exti.c.obj.d"
+  "/Users/yaroslavsvitlytskyi/STM32CubeIDE/workspace_1.14.0/accelerometer/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_flash.c" "CMakeFiles/accelerometer.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_flash.c.obj" "gcc" "CMakeFiles/accelerometer.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_flash.c.obj.d"
+  "/Users/yaroslavsvitlytskyi/STM32CubeIDE/workspace_1.14.0/accelerometer/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_flash_ex.c" "CMakeFiles/accelerometer.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_flash_ex.c.obj" "gcc" "CMakeFiles/accelerometer.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_flash_ex.c.obj.d"
+  "/Users/yaroslavsvitlytskyi/STM32CubeIDE/workspace_1.14.0/accelerometer/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_flash_ramfunc.c" "CMakeFiles/accelerometer.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_flash_ramfunc.c.obj" "gcc" "CMakeFiles/accelerometer.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_flash_ramfunc.c.obj.d"
+  "/Users/yaroslavsvitlytskyi/STM32CubeIDE/workspace_1.14.0/accelerometer/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_gpio.c" "CMakeFiles/accelerometer.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_gpio.c.obj" "gcc" "CMakeFiles/accelerometer.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_gpio.c.obj.d"
+  "/Users/yaroslavsvitlytskyi/STM32CubeIDE/workspace_1.14.0/accelerometer/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_i2c.c" "CMakeFiles/accelerometer.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_i2c.c.obj" "gcc" "CMakeFiles/accelerometer.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_i2c.c.obj.d"
+  "/Users/yaroslavsvitlytskyi/STM32CubeIDE/workspace_1.14.0/accelerometer/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_i2c_ex.c" "CMakeFiles/accelerometer.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_i2c_ex.c.obj" "gcc" "CMakeFiles/accelerometer.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_i2c_ex.c.obj.d"
+  "/Users/yaroslavsvitlytskyi/STM32CubeIDE/workspace_1.14.0/accelerometer/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_pwr.c" "CMakeFiles/accelerometer.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_pwr.c.obj" "gcc" "CMakeFiles/accelerometer.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_pwr.c.obj.d"
+  "/Users/yaroslavsvitlytskyi/STM32CubeIDE/workspace_1.14.0/accelerometer/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_pwr_ex.c" "CMakeFiles/accelerometer.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_pwr_ex.c.obj" "gcc" "CMakeFiles/accelerometer.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_pwr_ex.c.obj.d"
+  "/Users/yaroslavsvitlytskyi/STM32CubeIDE/workspace_1.14.0/accelerometer/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_rcc.c" "CMakeFiles/accelerometer.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_rcc.c.obj" "gcc" "CMakeFiles/accelerometer.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_rcc.c.obj.d"
+  "/Users/yaroslavsvitlytskyi/STM32CubeIDE/workspace_1.14.0/accelerometer/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_rcc_ex.c" "CMakeFiles/accelerometer.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_rcc_ex.c.obj" "gcc" "CMakeFiles/accelerometer.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_rcc_ex.c.obj.d"
+  "/Users/yaroslavsvitlytskyi/STM32CubeIDE/workspace_1.14.0/accelerometer/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_tim.c" "CMakeFiles/accelerometer.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_tim.c.obj" "gcc" "CMakeFiles/accelerometer.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_tim.c.obj.d"
+  "/Users/yaroslavsvitlytskyi/STM32CubeIDE/workspace_1.14.0/accelerometer/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_tim_ex.c" "CMakeFiles/accelerometer.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_tim_ex.c.obj" "gcc" "CMakeFiles/accelerometer.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_tim_ex.c.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/accelerometer.elf.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for accelerometer.elf.
+# This may be replaced when dependencies are built.
