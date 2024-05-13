08a029cb9cee6d4cd88521862666516d22df19d9
Yaroslav Svitlytskyi <yariksvitlitskiy81@gmail.com>
2024-05-13 02:09:47 +0200

    fix: fixed bugs

--- Scripts/cli/src/dto/__init__.py
+++ Scripts/cli/src/dto/__init__.py
@@ -2,5 +2,6 @@ from .available_devices_dto import AvailableDevicesDto
 
 from .retrieved_data_dto import DataTypeCompound
 from .retrieved_data_dto import RetrievedDataDto
+from .retrieved_data_dto import RawDataTypeValue
 
 from .visualizer_metadata_dto import VisualizerMetadataDto
